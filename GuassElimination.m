5505a815f313b1776b929b47aab7afd7833f4eb3
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2018-08-09 14:43:47 +0530

    Add files via upload

--- GuassElimination.m
+++ GuassElimination.m
@@ -9,16 +9,18 @@ else
     N = s_A(1);
     
     for i = 1:N-1
-        for k=i:N
-            if abs(A(i,i))<abs(A(k,i))
-                temp_row_A = A(i,:);
-                temp_row_b = b(i);
-                A(i,:) = A(k,:);
-                b(i) = b(k);
-                A(k,:) = temp_row_A;
-                b(k) = temp_row_b;
+        temp_index=i;
+        for k=i+1:N
+            if abs(A(i,i))<abs(A(k,i))                
+                temp_index=k;                
             end
         end
+        temp_row_A = A(i,:);
+        temp_row_b = b(i);
+        A(i,:) = A(temp_index,:);
+        b(i) = b(ktemp_index);
+        A(temp_index,:) = temp_row_A;
+        b(temp_index) = temp_row_b;
         
         for j = i+1:N
             m = -1*A(j,i)/A(i,i);
